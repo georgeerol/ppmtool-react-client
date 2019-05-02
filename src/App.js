b313ba3297e79f23009b0e6655fd5d11e0019d1e
George Fouche <fouliex@gmail.com>
2019-05-02 10:52:50 -0700

    Add react router, first functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./App.css";
-import Dashboard from "./Dashboard";
+import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./Project/AddProject";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Dashboard />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
       </div>
     </Router>
   );
